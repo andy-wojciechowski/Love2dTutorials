195093973b7744a20883491de94cd76b37fb81f0
andy-wojciechowski <31997141+andy-wojciechowski@users.noreply.github.com>
2018-04-17 19:57:06 -0500

    Started to hookup the tube to the main driver

--- Bird/entities/tube.lua
+++ Bird/entities/tube.lua
@@ -2,17 +2,18 @@ local class = require "libs.middleClass"
 
 local Tube = class('Tube')
 
-function Tube:initialize(x, y, width, spaceHeight)
+function Tube:initialize(x, y, width, height, spaceHeight)
     self.x = x
     self.y = y
     self.width = width
+    self.height = height
     self.spaceHeight = spaceHeight
 end
 
 function Tube:draw()
-    love.graphics.setColor(0, 1, 0)
-    love.graphics.rectangle('fill', self.x, self.y, self.width, self.y)
-    love.graphics.rectangle('fill', self.x, self.y + self.spaceHeight, self.width, self.y)
+    love.graphics.setColor(.37, .82, .28)
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+    --love.graphics.rectangle('fill', self.x, self.y + self.spaceHeight, self.width, self.y)
 end
 
 return Tube
--- Bird/main.lua
+++ Bird/main.lua
@@ -1,15 +1,29 @@
-Player = require "entities.player"
+local Player = require "entities.player"
+local Tube = require "entities.tube"
 
-gravity = -400
+local gravity = -400
 local playerInstance = Player:new()
+local tubes = {}
+
+function love.load()
+    createDefaultTubes()
+end
 
 function love.update(dt)
     jump = love.keyboard.isDown('space')
     playerInstance:updatePosition(jump, gravity, dt)
+    for i = 1, 2 do
+        tubes[i]:draw()
+    end
 end
 
 function love.draw()
     love.graphics.setColor(.14, .36, .46)
     love.graphics.rectangle('fill', 0, 0, 300, 388)
     playerInstance:draw()
+end
+
+function createDefaultTubes()
+    tubes[1] = Tube:new(100, 62, 388, 30)
+    tubes[2] = Tube:new(200, 62, 388, 30)
 end
